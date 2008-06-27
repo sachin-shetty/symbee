c2a3962b2608276bcd8c758f54133e754c2f66a5
sachin.shetty <sachin.shetty@5ee6f42f-8150-0410-aaef-a5b6308f0f4c>
2008-06-27 05:37:31 +0000

    committing message

--- src/scripts/startserver.sh
+++ src/scripts/startserver.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "LIB_DIR is set to: $LIB_DIR" 
+echo "SERVER_JAR is set to: $SERVER_JAR" 
 echo "KEY_STORE is set to: $KEY_STORE" 
 
 for i in `ls $LIB_DIR`
@@ -9,12 +10,15 @@ CLASSPATH=$CLASSPATH:$LIB_DIR/$i
 export CLASSPATH
 done
 
+CLASSPATH=$SERVER_JAR:$CLASSPATH
+export CLASSPATH
+
 echo "Classpath= $CLASSPATH"
 
-cd $LIB_DIR/build/log
-rm *
 #nohup java  -Djavax.net.ssl.keyStore=/home/sshetty/classes/vayoodoot/trunk/scripts/myKeystore -Djavax.net.ssl.keyStorePassword=sachin com.vayoodoot.server.MainServer 1522 2>> /home/sshetty/classes/vayoodoot/trunk/build/log/err.log 1>> /home/sshetty/classes/vayoodoot/trunk/build/log/out.log &
-nohup java  -Djavax.net.ssl.keyStore=$KEY_STORE -Djavax.net.ssl.keyStorePassword=$KEY_STORE com.vayoodoot.server.MainServer 1522 2>> /tmp/err.log 1>> /tmp/out.log &
+nohup java  -Djavax.net.ssl.keyStore=$KEY_STORE -Djavax.net.ssl.keyStorePassword=$KEY_STORE com.vayoodoot.server.MainServer 19000 & 
+
+
 
 sleep 3
 
